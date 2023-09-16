cfdd57ea96d6f6f54ce964b98c23572b4e5f1c6a
Subpoened-peon <www.nic.merritt@gmail.com>
2023-09-16 14:53:03 -0600

    Created DFAState, added unimplemented DFA methods.

--- p1Files/fa/dfa/DFA.java
+++ p1Files/fa/dfa/DFA.java
@@ -1,36 +1,41 @@
 package fa.dfa;
 
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.HashMap;
 import java.util.Map;
 
 import fa.State;
 
 public class DFA implements DFAInterface{
 
-    private Collection Q = new Collection();
-
-    @Override
+    private TreeSet<State> States = new TreeSet<State>();
+    private TreeSet<State> FinalStates = new TreeSet<State>();
+    private Set<Character> Alphabet = new TreeSet<Character>();
+    private Map<String, Map<Character, String>> Transitions = new HashMap<String,Map<Character,String>>();
+    private String stStart;
+    
+    //Incomplete / Erroneous
     public boolean addState(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+        State stName = State(name); 
+        return States.add(name);
     }
-
-    @Override
+    //Incomplete / Erroneous
     public boolean setFinal(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setFinal'");
+        return FinalStates.add(name);
     }
-
-    @Override
+    //Incomplete / Erroneous
     public boolean setStart(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setStart'");
+        boolean check = false;
+        if (States.contains(name)) {
+            this.stStart = name;
+            check = true;
+        }
+        return check;
     }
 
-    @Override
     public void addSigma(char symbol) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addSigma'");
+        Alphabet.add(symbol);
     }
 
     @Override
@@ -39,10 +44,8 @@ public class DFA implements DFAInterface{
         throw new UnsupportedOperationException("Unimplemented method 'accepts'");
     }
 
-    @Override
     public Set<Character> getSigma() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSigma'");
+        return this.Alphabet;
     }
 
     @Override
@@ -56,11 +59,13 @@ public class DFA implements DFAInterface{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isFinal'");
     }
-
-    @Override
+    //Incomplete / Erroneous
     public boolean isStart(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isStart'");
+        boolean check = false;
+        if (name == stStart) {
+            check = true;
+        }
+        return check;
     }
 
     @Override
