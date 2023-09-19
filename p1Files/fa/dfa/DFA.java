ac042a0abf2b6e37212060310025de388b9845b8
Kai Sorensen <kaisorensen@u.boisestate.edu>
2023-09-19 12:57:20 -0600

    wrote some methods

--- p1Files/fa/dfa/DFA.java
+++ p1Files/fa/dfa/DFA.java
@@ -17,12 +17,16 @@ public class DFA implements DFAInterface{
     
     //Incomplete / Erroneous
     public boolean addState(String name) {
-        State stName = State(name); 
-        return States.add(name);
+        return States.add(new DFAState(name));
     }
     //Incomplete / Erroneous
     public boolean setFinal(String name) {
-        return FinalStates.add(name);
+        boolean check = false;
+        if (States.contains(name)) {
+            FinalStates.add(new DFAState(name));
+            check = true;
+        }
+        return check;
     }
     //Incomplete / Erroneous
     public boolean setStart(String name) {
@@ -40,25 +44,35 @@ public class DFA implements DFAInterface{
 
     @Override
     public boolean accepts(String s) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accepts'");
+        return Alphabet.contains(s) ? true : false;
     }
 
     public Set<Character> getSigma() {
         return this.Alphabet;
     }
 
+    //NOTE THE FOLLOWING
+    //I'm not sure what to do if the state does not exist
+    //The interface does not allow us to throw an error here
+    //I returned a new state with the name requested
     @Override
     public State getState(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getState'");
+        for(State state : States) {
+            if(state.getName().equals(name)){
+                return state;
+            }
+        }
+        return new DFAState(name);
     }
 
     @Override
     public boolean isFinal(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isFinal'");
+        for(State s : FinalStates) {
+            if(s.getName().equals(name)) return true;
+        }
+        return false;
     }
+
     //Incomplete / Erroneous
     public boolean isStart(String name) {
         boolean check = false;
--- p1Files/fa/dfa/DFAState.java
+++ p1Files/fa/dfa/DFAState.java
@@ -1,7 +1,16 @@
 package fa.dfa;
 
-import fa.State;
+import  fa.State;
 
-public class DFAState extends State{
-    
+public class DFAState /*hi this is kai*/ extends State{
+
+    public DFAState(String name) {
+        super(name);
+    }
+
+    public String transition(String a) {
+        return "";
+    }
+
+
 }
