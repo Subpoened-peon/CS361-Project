30f3f99d58245ec1af5fe802824e1ffd6febab34
Kai Sorensen <kaisorensen@u.boisestate.edu>
2023-09-25 18:26:00 -0600

    fixes some bugs

--- p1Files/fa/dfa/DFAState.java
+++ p1Files/fa/dfa/DFAState.java
@@ -2,10 +2,15 @@ package fa.dfa;
 
 import  fa.State;
 
-public class DFAState /*hi this is kai*/ extends State{
+public class DFAState /*hi this is kai*/ extends State implements Comparable<DFAState>{
 
     public DFAState(String name) {
         super(name);
+    }
+
+    @Override
+    public int compareTo(DFAState s) {
+        return s.getName().compareTo(this.getName());
     }
     
 }
